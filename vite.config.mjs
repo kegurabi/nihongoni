b653645487d804e2f776f5e68e0c7dd621fa5977
kegurabi <kegurabi@gmail.com>
2025-10-28 04:37:22 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base:'/nihongini'
-,
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isVercel = !!process.env.VERCEL;
+  // correct GitHub Pages repo name used in package.json homepage
+  const ghPagesBase = '/nihongoni/';
 
+  return {
+    base: isVercel ? './' : ghPagesBase,
+    plugins: [react()],
+  }
+})
